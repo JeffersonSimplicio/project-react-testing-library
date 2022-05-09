c6e001869f60e0096a2c1c7a79fb49ca70bb490e
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-05-09 18:46:07 -0300

    WIP - Requisito 2, criado teste que verifica se existe uma breve descrição do que é a pokedéx

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import { About } from '../components';
+// import renderWithRouter from './renderWithRouter';
+
+describe('Testes do componente About', () => {
+  test('Se contem impormações sobre a Pokedex', () => {
+    render(<About />);
+    const infoDex = screen.getByText(/a digital encyclopedia containing all/);
+    expect(infoDex).toBeInTheDocument();
+  });
+});
