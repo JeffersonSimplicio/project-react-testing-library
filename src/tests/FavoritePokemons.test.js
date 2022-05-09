ac4ea61fd97283255687bca1aa6e4ff5de7045f1
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-05-09 20:46:42 -0300

    Requisito 3 - Ok, adiciona pikachu aos favoritos, e checa se ele foi realmente adicionado aos favoritos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
-// import App from '../App';
+import App from '../App';
 import { FavoritePokemons } from '../components';
 
 describe('Testa o componente Favorites Pokémons', () => {
@@ -14,26 +14,26 @@ describe('Testa o componente Favorites Pokémons', () => {
     expect(noFavorites).toBeInTheDocument();
   });
 
-  // test('Os podemons favoritos estão salvos', () => {
-  //   renderWithRouter(<App />);
+  test('Os podemons favoritos estão salvos', () => {
+    renderWithRouter(<App />);
 
-  //   // Adicionando Pikachu
-  //   const moreDetails = screen.getByText('More details');
-  //   expect(moreDetails).toBeInTheDocument();
-  //   userEvent.click(moreDetails);
-  //   const titlePikachu = screen.getByRole(
-  //     'heading', { level: 2, name: 'Pikachu Details' },
-  //   );
-  //   expect(titlePikachu).toBeInTheDocument();
-  //   const addFavorites = screen.getByRole('checkbox');
-  //   expect(addFavorites).toBeInTheDocument();
-  //   userEvent.click(addFavorites);
+    // Adicionando Pikachu
+    const moreDetails = screen.getByText('More details');
+    expect(moreDetails).toBeInTheDocument();
+    userEvent.click(moreDetails);
+    const titlePikachu = screen.getByRole(
+      'heading', { level: 2, name: 'Pikachu Details' },
+    );
+    expect(titlePikachu).toBeInTheDocument();
+    const addFavorites = screen.getByRole('checkbox');
+    expect(addFavorites).toBeInTheDocument();
+    userEvent.click(addFavorites);
 
-  //   // Checa se Pikachu foi adicionado
-  //   const linkFavorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
-  //   expect(linkFavorite).toBeInTheDocument();
-  //   userEvent.click(linkFavorite);
-  //   const favorite = screen.getByText('Pikachu');
-  //   expect(favorite).toBeInTheDocument();
-  // });
+    // Checa se Pikachu foi adicionado
+    const linkFavorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(linkFavorite).toBeInTheDocument();
+    userEvent.click(linkFavorite);
+    const favorite = screen.getByText('Pikachu');
+    expect(favorite).toBeInTheDocument();
+  });
 });
