625771a59e77d55e0b69cf53f757d6b601dd2ff0
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-05-09 18:05:46 -0300

    WIP - Requisito 1, inplementado primeiro teste, que lida com a existencia dos Links Home, About e Favorites

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste do componente App', () => {
+  test('Possui um conjunto de Links fixos', () => {
+    renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    const linkFavorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    expect(linkHome).toBeInTheDocument();
+    expect(linkAbout).toBeInTheDocument();
+    expect(linkFavorite).toBeInTheDocument();
+  });
+
+  // test('Link "Home" leva para rota  "/"', () => {
+  //   const { history } = renderWithRouter(<App />);
+  //   const linkHome = screen.getByRole('link', { name: 'Home' });
+
+  //   userEvent.click(linkHome);
+
+  //   const { location: { pathname } } = history;
+  //   expect(pathname).toBe('/');
+  // });
+});
