be770a6d1d755c8b9d2f263200d99f5a260a8837
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-05-10 12:54:39 -0300

    WIP - Requisito 6 - Criado o teste que verifica se as informações do pokemon estão corretas

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste da componente', () => {
+  test('Renderiza as informações corretas do pokemon na tela', () => {
+    renderWithRouter(<App />);
+
+    const pokemonName = screen.getByText('Pikachu');
+    expect(pokemonName).toBeInTheDocument();
+
+    const pokemonType = screen.getAllByText('Electric');
+    expect(pokemonType[0]).toBeInTheDocument();
+
+    const pokemonWeight = screen.getByText('Average weight: 6.0 kg');
+    expect(pokemonWeight).toBeInTheDocument();
+
+    const imagePokemon = screen.getByAltText('Pikachu sprite');
+    expect(imagePokemon).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+    expect(imagePokemon).toBeInTheDocument();
+  });
+
+  // test('Card opssui um link para mais detalhes', () => {
+  //   renderWithRouter(<App />);
+
+  //   const test = screen.getByRole('link', { name: 'More details' });
+  //   expect(test).toHaveAttribute('href', '/pokemons/25');
+  // });
+});
