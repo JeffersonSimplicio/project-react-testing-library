70a63d3d806e340d013ded12803a573dfaf1462b
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-05-10 09:22:19 -0300

    WIP - Requisito 5 - Criado o teste que verifica se o titulo h2 com o texto "Encountered pokémons" esta sendo exibido na tela

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testes da componente pokedex', () => {
+  test('Possui um h2 com o texto "Encountered pokémons"', () => {
+    renderWithRouter(<App />);
+    const title = screen.getByRole('heading', { level: 2, name: 'Encountered pokémons' });
+    expect(title).toBeInTheDocument();
+  });
+
+  // test('Todos os pokemons são listados ao clicar em proximo', () => {
+  //   renderWithRouter(<App />);
+
+  //   const pokemonListPartOne = ['Pikachu', 'Charmander', 'Caterpie', 'Ekans', 'Alakazam'];
+  //   const pokemonListPartTwo = ['Mew', 'Rapidash', 'Snorlax', 'Dragonair', 'Pikachu'];
+  //   const pokemonList = pokemonListPartOne.concat(pokemonListPartTwo);
+
+  //   pokemonList.forEach((pokemon) => {
+  //     // Procurar o botão dentro do for para garantir que ele não sumiria
+  //     const nextButton = screen.getByRole('button', { name: 'Próximo pokémon' });
+  //     expect(nextButton).toBeInTheDocument();
+
+  //     const namePokemon = screen.getByTestId('pokemon-name');
+  //     expect(namePokemon).toBeInTheDocument();
+  //     console.log(namePokemon.innerHTML);
+  //     expect(namePokemon.innerHTML).toBe(pokemon);
+
+  //     userEvent.click(nextButton);
+  //   });
+  // });
+});
