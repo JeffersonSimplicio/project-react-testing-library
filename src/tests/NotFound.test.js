2674f447ba7bd0f0fb8303c90e971ea416d2642f
Jefferson Simplicio <jeffersonsimplicio.js@gmail.com>
2022-05-09 21:02:17 -0300

    WIP - Requisito 4 - Criado o teste que verifica se o texto "Page requested not found" esta sendo renderizado

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+// import App from '../App';
+import { NotFound } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste do componente Not Found', () => {
+  test('O texto "Page requested not found 😭" esta sendo renderizado na tag H2', () => {
+    renderWithRouter(<NotFound />);
+    const textNotFound = screen.getByRole(
+      'heading', { level: 2, name: /Page requested not found/ },
+    );
+    expect(textNotFound).toBeInTheDocument();
+  });
+});
